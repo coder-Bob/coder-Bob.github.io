73ba2ca8872db63dc3ee3846a0fb86210a84a6f5
Bob <coderSkin@gmail.com>
2019-06-05 15:19:59 +0800

    v3

---
+++ react-tuanzi/precache-manifest.61ae1a5d5167fe20f42dfd0507803d3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c86d09c83c3cc678803e27c4f69f3dc",
+    "url": "/react-tuanzi/index.html"
+  },
+  {
+    "revision": "aa5ff19624607a2666bf",
+    "url": "/react-tuanzi/static/css/2.6afed8d2.chunk.css"
+  },
+  {
+    "revision": "ebc51d8f31a1ff2f3a2b",
+    "url": "/react-tuanzi/static/css/main.4469268e.chunk.css"
+  },
+  {
+    "revision": "aa5ff19624607a2666bf",
+    "url": "/react-tuanzi/static/js/2.ab3ff3d3.chunk.js"
+  },
+  {
+    "revision": "ebc51d8f31a1ff2f3a2b",
+    "url": "/react-tuanzi/static/js/main.10e4e806.chunk.js"
+  },
+  {
+    "revision": "dc7c52183d1e90d65c90",
+    "url": "/react-tuanzi/static/js/runtime~main.e126414c.js"
+  },
+  {
+    "revision": "3f788074c84c3826ff39b14e250e098b",
+    "url": "/react-tuanzi/static/media/iconfont.3f788074.woff"
+  },
+  {
+    "revision": "d0fdfee6f61872edae9e79e092f4cd79",
+    "url": "/react-tuanzi/static/media/iconfont.d0fdfee6.svg"
+  },
+  {
+    "revision": "d4cf4b5d5b096e3b19ccb2d2426e38b4",
+    "url": "/react-tuanzi/static/media/iconfont.d4cf4b5d.ttf"
+  },
+  {
+    "revision": "d778e278fdd99c9d4232b82c08936459",
+    "url": "/react-tuanzi/static/media/iconfont.d778e278.eot"
+  }
+]);
--- react-tuanzi/service-worker.js
+++ react-tuanzi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ae668f55cf9d99f3e76f324cf0f4c8de.js"
+  "/react-tuanzi/precache-manifest.61ae1a5d5167fe20f42dfd0507803d3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tuanzi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
