4431428fe41fee6b394ed04d80c55f8dbee15c94
Bob <coderSkin@gmail.com>
2019-06-05 15:13:15 +0800

    修改文件调用路径

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 感谢github提供免费的域名及空间，本项目用来存放个人平日工作中接触到的案例。
 
 项目一：[一个基于vue的后台管理系统](http://coder-Bob.github.io/vue-backstage/ "vue后台"). 
+
+项目二：[一个基于React的生鲜电商小页面](http://coder-Bob.github.io/react-tuanzi/ "生鲜电商"). 
--- react-tuanzi/service-worker.js
+++ react-tuanzi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f366f8f39e65b43253bf558bbb8e50b5.js"
+  "./precache-manifest.ae668f55cf9d99f3e76f324cf0f4c8de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
