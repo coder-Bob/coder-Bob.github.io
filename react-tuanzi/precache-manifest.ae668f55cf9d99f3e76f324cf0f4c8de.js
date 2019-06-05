b2a720d77e3e4046eaf0f6a2b26b67ba8d7ce5a6
Bob <coderSkin@gmail.com>
2019-06-05 15:15:49 +0800

    添加文件

---
+++ react-tuanzi/precache-manifest.ae668f55cf9d99f3e76f324cf0f4c8de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51577ce0cdeaf65cf695a961697d4131",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa5ff19624607a2666bf",
+    "url": "./static/css/2.6afed8d2.chunk.css"
+  },
+  {
+    "revision": "4e2d1a9fa763d0b69c8a",
+    "url": "./static/css/main.7f5fa587.chunk.css"
+  },
+  {
+    "revision": "aa5ff19624607a2666bf",
+    "url": "./static/js/2.ab3ff3d3.chunk.js"
+  },
+  {
+    "revision": "4e2d1a9fa763d0b69c8a",
+    "url": "./static/js/main.2224445e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3f788074c84c3826ff39b14e250e098b",
+    "url": "./static/media/iconfont.3f788074.woff"
+  },
+  {
+    "revision": "d0fdfee6f61872edae9e79e092f4cd79",
+    "url": "./static/media/iconfont.d0fdfee6.svg"
+  },
+  {
+    "revision": "d4cf4b5d5b096e3b19ccb2d2426e38b4",
+    "url": "./static/media/iconfont.d4cf4b5d.ttf"
+  },
+  {
+    "revision": "d778e278fdd99c9d4232b82c08936459",
+    "url": "./static/media/iconfont.d778e278.eot"
+  }
+]);
